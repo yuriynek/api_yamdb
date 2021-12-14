2e284e95b0ff3c89cfaf0fe684ee22272c99a8e4
Юрий Некрасов <iury.nekrasov@yandex.ru>
2021-12-14 23:15:21 +0300

    test commit

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+SCORES = [i for i in range(1, 11)]
+
+
+class Review(models.Model):
+    title = models.ForeignKey('Title',
+                              verbose_name='Произведение',
+                              on_delete=models.CASCADE,
+                              related_name='reviews')
+    text = models.TextField(verbose_name='Текст отзыва')
+    author = models.ForeignKey('User',
+                               verbose_name='Автор',
+                               on_delete=models.CASCADE,
+                               related_name='reviews',
+                               db_column='author')
+    score = models.IntegerField(verbose_name='Оценка',
+                                choices=SCORES)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review,
+                               on_delete=models.CASCADE,
+                               related_name='comments')
+    text = models.TextField(verbose_name='Текст комментария')
+    author = models.ForeignKey('User',
+                               verbose_name='Автор',
+                               on_delete=models.CASCADE,
+                               related_name='reviews',
+                               db_column='author')
+    pub_date = models.DateTimeField(auto_now_add=True)
