6e8ade2e66ab3db5457d41744e43401172389d56
kyo508 <alexeyka-93@mail.ru>
2021-12-16 01:24:54 +0300

    models added

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -5,32 +5,36 @@ SCORES = [i for i in range(1, 11)]
 
 
 class User(AbstractUser):
-    bio = models.TextField(
-        'Биография',
-        blank=True,
-    )
+    bio = models.TextField('Биография',
+                           blank=True,)
     role = models.CharField(choices=...)
 
 
-
 class Category(models.Model):
-    slug = models.SlugField(unique=True)
-
+    name = models.TextField(max_length=256, verbose_name='Название категории')
+    slug = models.SlugField(max_length=50, unique=True)
 
 
 class Genre(models.Model):
+    name = models.TextField(verbose_name='Название жанра')
     slug = models.SlugField(unique=True)
 
 
 class Title(models.Model):
-    name = models.TextField(max_length=255, verbose_name="name")
-    year = models.IntegerField()
-    category = models.ForeignKey(Category, verbose_name="Category")
+    name = models.TextField(max_length=255,
+                            verbose_name='Название произведения')
+    year = models.IntegerField(verbose_name='Год выпуска')
+    description = models.TextField(max_length=255, verbose_name='Описание')
     genre = models.ForeignKey(Genre,
                               on_delete=models.SET_NULL,
-
-                              )
-
+                              verbose_name='Жанр',
+                              blank=True,
+                              null=True)
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 verbose_name='Категория',
+                                 blank=True,
+                                 null=True)
 
 
 class Review(models.Model):
