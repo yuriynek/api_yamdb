3da579ff105b27a2b01580656fa039f6a6a2c60f
Юрий Некрасов <iury.nekrasov@yandex.ru>
2021-12-15 22:02:46 +0300

    added urls, initial viewsets and permissions

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,30 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnlyPermission(permissions.BasePermission):
+    message = 'Права на изменение данного контента принадлежат администратору'
+
+    def has_object_permission(self, request, view, obj):
+        """Проверка. Если запрос только на чтение, то разрешаем его.
+        Если на изменение - проверяем, является ли пользователь админом"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.role == 'admin'
+
+
+class IsAuthorOrStaffOrReadOnlyPermission(permissions.BasePermission):
+    message = ('Права на изменение данного контента принадлежат '
+               'администраторам, модераторам либо авторам записей')
+
+    def has_object_permission(self, request, view, obj):
+        """Проверка. Если запрос только на чтение, то разрешаем его.
+        Если пользователь авторизован - разрешает создание записи
+        Если запрос на изменение - проверяем, является ли пользователь админом/модератором/автором"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.method == 'POST':
+            return request.user.is_authenticated
+        return any([request.user.role == 'admin',
+                    request.user.role == 'moderator',
+                    obj.author == request.user
+                    ])
